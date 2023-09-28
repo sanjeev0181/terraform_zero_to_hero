2b182386be64dca8680d2b2b871f09032eea816b
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-09-28 12:09:04 +0530

    initial files for day-1

--- Day01/PROJECT-ec2-instance-creation/main.tf
+++ Day01/PROJECT-ec2-instance-creation/main.tf
@@ -6,4 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-053b0d53c279acc90"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-0290eca61e6a7eb79"
+    key_name = "terraform"
 }
